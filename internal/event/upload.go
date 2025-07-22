b655eb2c9e03e2ac550f31f55e3d44bd398f67ce
ak <akashcsemu@gmail.com>
2025-07-22 11:27:01 +0600

    Event And Chanel Setup Done

--- internal/event/upload.go
+++ internal/event/upload.go
@@ -3,12 +3,14 @@ package event
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func UploadFile(idx int, parentWg *sync.WaitGroup, validatedMessages chan Event) {
 	defer parentWg.Done()
 	for msg := range validatedMessages {
 		fmt.Println("Uploading validated message:", msg, "at index:", idx)
+		time.Sleep(1 * time.Second)
 	}
 	fmt.Println("No more messages to upload.", "at index:", idx)
 }
